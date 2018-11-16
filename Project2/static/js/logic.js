30f53f0c78c8f70c68ea7b1ee9b19664aa7053cb
Vikas <vikassingheng@gmail.com>
2018-11-15 20:24:26 -0600

    With Election 2016 Map

--- Project2/static/js/logic.js
+++ Project2/static/js/logic.js
@@ -1,32 +1,85 @@
-// Creating map object
-var myMap = L.map("map", {
-  center: [43.7844, -88.7879],
-  zoom: 7
+
+var csvData = [];
+
+
+d3.csv("data/wi_2016.csv", function (data) {
+
+  data.forEach(function (d) {
+    csvData.push(d);
+
+  });
+
+
+
+  // Creating map object
+  var myMap = L.map("map", {
+    center: [44.7844, -88.7879],
+    zoom: 7
+  });
+
+  // Adding tile layer
+  L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  function selectColor(county) {
+
+    var countyData = csvData.filter(x => (x.county_name.includes(county)))
+
+    let dem = +countyData[0].votes_dem;
+    let gop = +countyData[0].votes_gop;
+
+    let diff = dem - gop;
+    console.log("diff " + diff);
+    let clr ;
+    if (diff > 0) {
+      clr = "blue";
+    } else {
+      clr = "red";
+    }
+    let opacity;
+    let absDiff = Math.abs(diff);
+    if(absDiff > 100000){
+      opacity = 0.9;
+    } else if(absDiff <= 100000 && absDiff > 75000){
+      opacity = 0.8;
+    }else if(absDiff <= 75000 && absDiff > 30000){
+      opacity = 0.6;
+    }else if(absDiff <= 30000 && absDiff > 5000){
+      opacity = 0.5;
+    } else if(absDiff <= 5000 && absDiff > 1000){
+      opacity = 0.4;
+    }else {
+      opacity = 0.2;
+    }
+    return [clr, opacity];
+
+  }
+
+  // Grabbing our GeoJSON data..
+  d3.json("data/WI.geojson", function (data) {
+    //console.log("geoJson" + JSON.stringify(data))
+    L.geoJson(data, {
+      style: function (feature) {
+        let clr = selectColor(feature.properties.COUNTY_NAME);
+
+        return {
+          color: "white",
+          fillColor: clr[0],
+          fillOpacity: clr[1],
+          weight: 1.5
+        }
+      }
+    }).addTo(myMap);
+  });
+
+
 });
 
-// Adding tile layer
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.streets",
-  accessToken: API_KEY
-}).addTo(myMap);
 
 
-
-// Grabbing our GeoJSON data..
-d3.json("data/WI.geojson", function(data) {
-  // Creating a GeoJSON layer with the retrieved data
-  console.log("geoJson" + JSON.stringify(data))
-
-  L.geoJson(data).addTo(myMap);
-});
-
-// d3.csv("data/wi_2016.csv", function(data){
-
-//   console.log("csv data " + data)
-
-// })
 // Link to GeoJSON
 
 var geojson;
